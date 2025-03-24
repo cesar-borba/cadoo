dba452b5a1383b421dd05aa5b46d9a1a00ead8d1
César Borba <cesarmartins350@gmail.com>
2025-03-24 00:18:55 -0300

    Alterando credencial de teste

--- assets/js/login-validacao.js
+++ assets/js/login-validacao.js
@@ -3,7 +3,7 @@ function logar() {
     const senha = document.getElementById("senha").value;
 
     // Verifica se o email e a senha são os corretos
-    if (email === "kaue@email.com" && senha === "123") {
+    if (email === "cadoo@email.com" && senha === "123") {
         // Redireciona para a página Principal.html
         location.href = "pages/principal.html";
     }
